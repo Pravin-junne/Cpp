cc63a7eb3c8157ca17084a2d9803e53b7de93fd3
Pravin junne <pravinjunne@gmail.com>
2023-07-16 14:21:53 +0530

    Simple i/o program

---
+++ Cpp_4.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    
+    int num1,num2;
+    cout<<"Enter the number 1:\n";/* '<<' is called Insertion oprator*/
+    cin>>num1;
+    cout<<"Enter the number 2:\n";
+    cin>>num2;  /* '>>' is called Extraction oprator*/
+    cout<<"Sum is:"<<num1+num2;
+    return 0;
+}
