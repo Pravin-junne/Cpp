2d84003bac124b8c79a0c981672aee45a1e68a6c
Pravin junne <pravinjunne@gmail.com>
2023-07-16 13:53:48 +0530

    Variable with Local scope

---
+++ Cpp_2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //variables with local scope..
+    int a = 10, b = 20; 
+    float x = 10.10;
+    char z = 'A';
+    cout<<"Value of integer a is:"<<a<<"\nValue of b is:"<<b;
+    cout<<"\nValue of float x is:"<<x<<"\nValue of charactor z is"<<z;
+    return 0;
+}
