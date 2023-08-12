cd45554c878ac770c61cb721f06b7a65256a7be7
Pravin junne <pravinjunne@gmail.com>
2023-08-12 15:56:20 +0530

    constant variable

---
+++ Cpp_7.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+    const float pi = 3.14;  //we can not change value of pi because we made it constant: const
+    cout<<pi; //3.14
+    return 0;
+}
