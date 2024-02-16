4cb313ee802abe2fbdc30f62ecd207ab8a8ac186
Pravin junne <128027999+Pravin-junne@users.noreply.github.com>
2024-02-16 16:08:38 +0530

    Pointers in Cpp

---
+++ Cpp_10.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+        //&-->(Address of) operator
+        //*-->(value at) Dereference operator.
+    int a=3;
+    int* b=&a;
+    cout<<"The address of a is: "<<&a<<endl;
+    cout<<"The address of a is  :"<<b<<endl;
+    cout<<"The value of b:"<<*b<<endl;//value stored at pointer
+
+    //pointer to pointer
+    int** c = &b;
+    cout<<"The address of a b is:"<<&b<<endl;
+    cout<<"The address of b is "<<c<<endl;
+    cout<<"The value at c : "<<**c<<endl;
+    return 0;
+}
