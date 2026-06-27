a3a9d7c39f45f24ae58ee12fb1b5c258b318cca5
Pravin junne <128027999+Pravin-junne@users.noreply.github.com>
2026-06-27 18:23:16 +0530

    Encapsulation Getter and Setter to access private members of class

---
+++ OOP/Encapsulation_01.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+class Employee {
+    private:
+        //private attribute
+        int salary;
+
+    public:
+        //setter
+        void setSalary(int s) {
+            salary = s;
+        }
+
+        //Getter
+        int getSalary() {
+            return salary;
+        }
+};
+
+int main() {
+    Employee myObj;
+    myObj.setSalary(50000);
+    cout << myObj.getSalary();
+
+    return 0;
+}
